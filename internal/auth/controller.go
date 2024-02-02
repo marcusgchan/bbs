81a31e773ff58e6d63b75f699680899b53ccd35f
Marcus Chan <marcuscchan@gmail.com>
2024-02-01 18:37:42 -0800

    Install jwt and uuid

--- internal/auth/controller.go
+++ internal/auth/controller.go
@@ -19,5 +19,30 @@ func (h AuthHandler) Login(c echo.Context) error {
 		return internal.Render(auth.LoginPage(), c)
 	}
 	fmt.Printf("token: %s\n", token)
-	return nil
+	return c.Redirect(302, "/")
+}
+
+func (h AuthHandler) HandleLogin(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	type User struct {
+		ID       int
+		Username string
+		Password string
+	}
+
+	user := User{}
+
+	err := h.DB.QueryRow("SELECT * FROM users WHERE username = $1", username).Scan(&user.ID, &user.Username, &user.Password)
+	defer h.DB.Close()
+	if err != nil {
+		return c.String(401, "Unauthorized")
+	}
+
+	if user.Password != password {
+		return c.String(401, "Unauthorized")
+	}
+
+	return c.Redirect(302, "/")
 }
